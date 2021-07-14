7625c188c6d13427526338b6054e966e160c170d
Miriam Aceves <miriammerari02@gmail.com>
2021-07-14 17:11:16 -0400

    Create open mic class, initialize and create welcome method

---
+++ lib/open_mic.rb
@@ -0,0 +1,13 @@
+class OpenMic
+  attr_reader :location, :date, :performers
+
+  def initialize(info)
+    @location = info[:location]
+    @date = info[:date]
+    @performers = []
+  end
+
+  def welcome(user)
+    @performers << user
+  end
+end
